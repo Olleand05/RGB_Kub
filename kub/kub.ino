9f0816b9aa70d4bdcd532e73fd0bc1493aad0ecc
Olleand05 <149485272+Olleand05@users.noreply.github.com>
2024-03-29 18:23:34 +0100

    Update kub.ino

--- kub/kub.ino
+++ kub/kub.ino
@@ -4,6 +4,13 @@
 // both included here to make things simple for example
 #include <Adafruit_MCP23X17.h>
 
+const char layer1 = 4;
+const char layer2 = 5;
+const char layer3 = 6;
+const char layer4 = 7;
+
+int fun1tick = 200;
+int timer1 = 0;
 
 // uncomment appropriate line
 //Adafruit_green3X08 mcp;
@@ -11,12 +18,25 @@ Adafruit_MCP23X17 red;
 Adafruit_MCP23X17 green;
 Adafruit_MCP23X17 blue;
 
-int Layer[] = {4,5,6,7};
-int LampTower[4][4]={
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0},
-  {0,0,0,0}
+int layerRed[4][4] = {
+  { 2, 4, 10, 11 },
+  { 3, 0, 6, 12 },
+  { 14, 15, 7, 9 },
+  { 1, 13, 5, 8 }
+};
+
+int layerGreen[4][4] = {
+  { 2, 5, 0, 1 },
+  { 8, 10, 7, 14 },
+  { 9, 15, 12, 3 },
+  { 11, 6, 4, 13 }
+};
+
+int layerBlue[4][4] = {
+  { 6, 0, 1, 2 },
+  { 7, 13, 8, 9 },
+  { 3, 4, 11, 12 },
+  { 5, 15, 14, 10 }
 };
 
 void setup() {
@@ -31,41 +51,423 @@ void setup() {
 
   // configure pin for output
   for (int i = 0; i < 16; i++) {
-    red.pinMode(i, HIGH);
+    red.pinMode(i, OUTPUT);
   }
   for (int i = 0; i < 16; i++) {
-    green.pinMode(i, HIGH);
+    green.pinMode(i, OUTPUT);
   }
   for (int i = 0; i < 16; i++) {
-    blue.pinMode(i, HIGH);
+    blue.pinMode(i, OUTPUT);
   }
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(layer1, OUTPUT);
+  pinMode(layer2, OUTPUT);
+  pinMode(layer3, OUTPUT);
+  pinMode(layer4, OUTPUT);
 
-  Serial.println("Looping...");
+  digitalWrite(layer1, LOW);
+  digitalWrite(layer2, LOW);
+  digitalWrite(layer3, LOW);
+  digitalWrite(layer4, LOW);
 }
 
 void loop() {
-  LightAllLamps(blue=true, red=true, green = true);
+  allOff();
+  //function1();
+
+  //lightSomeLampsPatternByOlleSigmaLord();
+
+  crazyFrog();
 }
 
-void LightAllLamps(bool red, bool blue, bool green){
-  for(int i = 0;i<4;i++){
-    writeDigital(Layer[i], HIGH);
-  }
-  for(int i=0;i<16;i++){
-    if(red){
-      red.digitalWrite(i,LOW);
-    }
-    if(blue){
-      blue.digitalWrite(i,LOW);
-    }
-    if(green){
-      green.digitalWrite(i,LOW);
-    }
+void LightAllLamps() {
+}
 
+void function1() {
+  digitalWrite(layer1, HIGH);
+  function1pattern();
+  allOff();
+
+  digitalWrite(layer1, HIGH);
+  digitalWrite(layer2, HIGH);
+  function1pattern();
+  allOff();
+
+  digitalWrite(layer1, HIGH);
+  digitalWrite(layer2, HIGH);
+  digitalWrite(layer3, HIGH);
+  function1pattern();
+  allOff();
+
+  digitalWrite(layer1, HIGH);
+  digitalWrite(layer2, HIGH);
+  digitalWrite(layer3, HIGH);
+  digitalWrite(layer4, HIGH);
+  function1pattern();
+  allOff();
+}
+
+void function1pattern() {
+  allColumnsOff();
+  red.digitalWrite(layerRed[0][0], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  green.digitalWrite(layerGreen[0][0], LOW);
+  red.digitalWrite(layerRed[1][0], LOW);
+  red.digitalWrite(layerRed[0][1], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  blue.digitalWrite(layerBlue[0][0], LOW);
+  green.digitalWrite(layerGreen[1][0], LOW);
+  green.digitalWrite(layerGreen[0][1], LOW);
+  red.digitalWrite(layerRed[2][0], LOW);
+  red.digitalWrite(layerRed[1][1], LOW);
+  red.digitalWrite(layerRed[0][2], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  red.digitalWrite(layerRed[0][0], LOW);
+  blue.digitalWrite(layerBlue[1][0], LOW);
+  blue.digitalWrite(layerBlue[0][1], LOW);
+  green.digitalWrite(layerGreen[2][0], LOW);
+  green.digitalWrite(layerGreen[1][1], LOW);
+  green.digitalWrite(layerGreen[0][2], LOW);
+  red.digitalWrite(layerRed[3][0], LOW);
+  red.digitalWrite(layerRed[0][3], LOW);
+  red.digitalWrite(layerRed[2][1], LOW);
+  red.digitalWrite(layerRed[1][2], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  green.digitalWrite(layerGreen[0][0], LOW);
+  red.digitalWrite(layerRed[1][0], LOW);
+  red.digitalWrite(layerRed[0][1], LOW);
+  blue.digitalWrite(layerBlue[2][0], LOW);
+  blue.digitalWrite(layerBlue[1][1], LOW);
+  blue.digitalWrite(layerBlue[0][2], LOW);
+  green.digitalWrite(layerGreen[3][0], LOW);
+  green.digitalWrite(layerGreen[0][3], LOW);
+  green.digitalWrite(layerGreen[2][1], LOW);
+  green.digitalWrite(layerGreen[1][2], LOW);
+  red.digitalWrite(layerRed[3][1], LOW);
+  red.digitalWrite(layerRed[1][3], LOW);
+  red.digitalWrite(layerRed[2][2], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  blue.digitalWrite(layerBlue[0][0], LOW);
+  green.digitalWrite(layerGreen[1][0], LOW);
+  green.digitalWrite(layerGreen[0][1], LOW);
+  red.digitalWrite(layerRed[2][0], LOW);
+  red.digitalWrite(layerRed[1][1], LOW);
+  red.digitalWrite(layerRed[0][2], LOW);
+  blue.digitalWrite(layerBlue[3][0], LOW);
+  blue.digitalWrite(layerBlue[0][3], LOW);
+  blue.digitalWrite(layerBlue[2][1], LOW);
+  blue.digitalWrite(layerBlue[1][2], LOW);
+  green.digitalWrite(layerGreen[3][1], LOW);
+  green.digitalWrite(layerGreen[1][3], LOW);
+  green.digitalWrite(layerGreen[2][2], LOW);
+  red.digitalWrite(layerRed[2][3], LOW);
+  red.digitalWrite(layerRed[3][2], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  red.digitalWrite(layerRed[0][0], LOW);
+  blue.digitalWrite(layerBlue[1][0], LOW);
+  blue.digitalWrite(layerBlue[0][1], LOW);
+  green.digitalWrite(layerGreen[2][0], LOW);
+  green.digitalWrite(layerGreen[1][1], LOW);
+  green.digitalWrite(layerGreen[0][2], LOW);
+  red.digitalWrite(layerRed[3][0], LOW);
+  red.digitalWrite(layerRed[0][3], LOW);
+  red.digitalWrite(layerRed[2][1], LOW);
+  red.digitalWrite(layerRed[1][2], LOW);
+  blue.digitalWrite(layerBlue[3][1], LOW);
+  blue.digitalWrite(layerBlue[1][3], LOW);
+  blue.digitalWrite(layerBlue[2][2], LOW);
+  green.digitalWrite(layerGreen[2][3], LOW);
+  green.digitalWrite(layerGreen[3][2], LOW);
+  red.digitalWrite(layerRed[3][3], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  green.digitalWrite(layerGreen[0][0], LOW);
+  red.digitalWrite(layerRed[1][0], LOW);
+  red.digitalWrite(layerRed[0][1], LOW);
+  blue.digitalWrite(layerBlue[2][0], LOW);
+  blue.digitalWrite(layerBlue[1][1], LOW);
+  blue.digitalWrite(layerBlue[0][2], LOW);
+  green.digitalWrite(layerGreen[3][0], LOW);
+  green.digitalWrite(layerGreen[0][3], LOW);
+  green.digitalWrite(layerGreen[2][1], LOW);
+  green.digitalWrite(layerGreen[1][2], LOW);
+  red.digitalWrite(layerRed[3][1], LOW);
+  red.digitalWrite(layerRed[1][3], LOW);
+  red.digitalWrite(layerRed[2][2], LOW);
+  blue.digitalWrite(layerBlue[2][3], LOW);
+  blue.digitalWrite(layerBlue[3][2], LOW);
+  green.digitalWrite(layerGreen[3][3], LOW);
+  delay(fun1tick);
+
+  allColumnsOff();
+  blue.digitalWrite(layerBlue[0][0], LOW);
+  green.digitalWrite(layerGreen[1][0], LOW);
+  green.digitalWrite(layerGreen[0][1], LOW);
+  red.digitalWrite(layerRed[2][0], LOW);
+  red.digitalWrite(layerRed[1][1], LOW);
+  red.digitalWrite(layerRed[0][2], LOW);
+  blue.digitalWrite(layerBlue[3][0], LOW);
+  blue.digitalWrite(layerBlue[0][3], LOW);
+  blue.digitalWrite(layerBlue[2][1], LOW);
+  blue.digitalWrite(layerBlue[1][2], LOW);
+  green.digitalWrite(layerGreen[3][1], LOW);
+  green.digitalWrite(layerGreen[1][3], LOW);
+  green.digitalWrite(layerGreen[2][2], LOW);
+  red.digitalWrite(layerRed[2][3], LOW);
+  red.digitalWrite(layerRed[3][2], LOW);
+  blue.digitalWrite(layerBlue[3][3], LOW);
+  delay(fun1tick);
+}
+
+void lightSomeLampsPatternByOlleSigmaLord() {
+  digitalWrite(layer1, HIGH);
+  digitalWrite(layer2, HIGH);
+  digitalWrite(layer3, HIGH);
+  digitalWrite(layer4, HIGH);
+
+  int y[] = { 0, 0, 0, 0, 1, 2, 3, 3, 3, 3, 2, 1, 1, 1, 2, 2 };
+  int x[] = { 0, 1, 2, 3, 3, 3, 3, 2, 1, 0, 0, 0, 1, 2, 2, 1 };
+
+  for (int i = 0; i < 16; i++) {
+    blue.digitalWrite(layerBlue[y[i]][x[i]], HIGH);
+    green.digitalWrite(layerGreen[y[i]][x[i]], LOW);
+    delay(200);
+  }
+  delay(500);
+  for (int i = 0; i < 16; i++) {
+    green.digitalWrite(layerGreen[y[i]][x[i]], HIGH);
+    red.digitalWrite(layerRed[y[i]][x[i]], LOW);
+    delay(200);
+  }
+  delay(500);
+  for (int i = 0; i < 16; i++) {
+    red.digitalWrite(layerRed[y[i]][x[i]], HIGH);
+    blue.digitalWrite(layerBlue[y[i]][x[i]], LOW);
+    delay(200);
+  }
+} 
+
+void crazyFrog() {
+  digitalWrite(layer1, LOW);
+  digitalWrite(layer2, LOW);
+  digitalWrite(layer3, LOW);
+  digitalWrite(layer4, LOW);
+
+  digitalWrite(layer4, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      blue.digitalWrite(layerBlue[i][j], LOW);
+    }
   }
 
+  delay(200);
+
+  digitalWrite(layer4, LOW);
+  digitalWrite(layer3, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      green.digitalWrite(layerBlue[i][j], LOW);
+      blue.digitalWrite(layerBlue[i][j], LOW);
+    }
+  }
+
+  delay(200);
+
+  digitalWrite(layer3, LOW);
+  digitalWrite(layer2, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      green.digitalWrite(layerBlue[i][j], HIGH);
+      blue.digitalWrite(layerBlue[i][j], LOW);
+    }
+  }
+
+  delay(200);
+
+  digitalWrite(layer2, LOW);
+  digitalWrite(layer1, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      green.digitalWrite(layerGreen[i][j], LOW);
+      blue.digitalWrite(layerBlue[i][j], LOW);
+    }
+  }
+
+  delay(200);
+  allOff();
+
+  digitalWrite(layer1, HIGH);
+  digitalWrite(layer2, HIGH);
+  digitalWrite(layer3, HIGH);
+  digitalWrite(layer4, HIGH);
+
+
+  for(int i = 0; i<4;i++){
+    red.digitalWrite(layerRed[0][i], LOW);
+  }
+  delay(200);
+
+  for(int i = 0; i<4;i++){
+    green.digitalWrite(layerGreen[1][i], LOW);
+  }
+
+  delay(200);
+
+  for(int i = 0; i<4;i++){
+    red.digitalWrite(layerRed[2][i], LOW);
+    green.digitalWrite(layerGreen[2][i], HIGH);
+  }
+
+  delay(200);
+
+  for(int i = 0; i<4;i++){
+    green.digitalWrite(layerGreen[3][i], LOW);
+  }
+
+  delay(200); 
+
+  allOff();
+
+  delay(200);
+
+  digitalWrite(layer1, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      blue.digitalWrite(layerBlue[i][j], LOW);
+    }
+  }
+
+  delay(200);
+
+  digitalWrite(layer1, LOW);
+  digitalWrite(layer2, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      green.digitalWrite(layerGreen[i][j], LOW);
+      blue.digitalWrite(layerBlue[i][j], HIGH);
+    }
+  }
+
+  delay(200);
+
+  digitalWrite(layer2, LOW);
+  digitalWrite(layer3, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      green.digitalWrite(layerGreen[i][j], HIGH);
+      blue.digitalWrite(layerBlue[i][j], LOW);
+    }
+  }
+
+  delay(200);
+
+  digitalWrite(layer3, LOW);
+  digitalWrite(layer4, HIGH);
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      green.digitalWrite(layerGreen[i][j], LOW);
+      blue.digitalWrite(layerBlue[i][j], HIGH);
+    }
+  }
+
+  allOff();
+
+  for (int i = 0; i < 4; i++) {
+    red.digitalWrite(layerRed[3][i], LOW);
+  }
+  delay(200);
+
+  for (int i = 0; i < 4; i++) {
+    green.digitalWrite(layerGreen[2][i], LOW);
+    blue.digitalWrite(layerBlue[2][i], LOW);
+    red.digitalWrite(layerRed[2] [i], HIGH);
+  }
+
+  delay(200);
+
+  for (int i = 0; i < 4; i++) {
+   green.digitalWrite(layerGreen[1][i], HIGH);
+    blue.digitalWrite(layerBlue[1][i], HIGH);
+    red.digitalWrite(layerRed[1][i], LOW);
+  }
+
+  delay(200);
+
+  for (int i = 0; i < 4; i++) {
+    green.digitalWrite(layerGreen[0][i], LOW);
+    blue.digitalWrite(layerBlue[0][i], LOW);
+    red.digitalWrite(layerRed[0][i], HIGH);
+  }
+
+  delay(200);
+
+  allOff();
+}
+
+
+
+void testOrderBlue() {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      blue.digitalWrite(layerBlue[i][j], LOW);
+      delay(400);
+    }
+  }
+}
+void testOrderRed() {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      red.digitalWrite(layerRed[i][j], LOW);
+      delay(400);
+    }
+  }
+}
+void testOrderGreen() {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      green.digitalWrite(layerGreen[i][j], LOW);
+      delay(400);
+    }
+  }
+}
+
+void allOff() {
+  for (int i = 0; i < 16; i++) {
+    red.digitalWrite(i, HIGH);
+  }
+  for (int i = 0; i < 16; i++) {
+    green.digitalWrite(i, HIGH);
+  }
+  for (int i = 0; i < 16; i++) {
+    blue.digitalWrite(i, HIGH);
+  }
+  digitalWrite(layer1, LOW);
+  digitalWrite(layer2, LOW);
+  digitalWrite(layer3, LOW);
+  digitalWrite(layer4, LOW);
+}
+
+void allColumnsOff() {
+  for (int i = 0; i < 16; i++) {
+    red.digitalWrite(i, HIGH);
+  }
+  for (int i = 0; i < 16; i++) {
+    green.digitalWrite(i, HIGH);
+  }
+  for (int i = 0; i < 16; i++) {
+    blue.digitalWrite(i, HIGH);
+  }
+  
 }
